1bd8f729bb5427e1809ef9ab33f298af42d422de
Felipe Barcon <felipe.barcon@gmail.com>
2022-06-03 15:05:07 +0200

    Display search results in the browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApplicationController
 
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      # Except current user to te list of search users
+      @friends = current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -55,4 +55,9 @@ class User < ApplicationRecord
   def self.matches(field_name, param)
     where("#{field_name} like ?", "%#{param}%")
   end
+
+  # Except current user
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
 end
