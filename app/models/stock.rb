91d09ae3bb5728a4644ebfd51003b924b5869f0c
Felipe Barcon <felipe.barcon@gmail.com>
2022-06-04 15:52:49 +0200

    add price no stcoked at db

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -17,4 +17,14 @@ class Stock < ApplicationRecord
   def self.check_db(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
+
+  def api_call
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+  end
+
+  def price(ticker_symbol)
+    client = api_call
+    @price = client.price(ticker_symbol)
+  end
 end
